8eb5f173a52b8afb8c4fb124bfb134eae2deb17f
pinky-pig <2545589993@qq.com>
2022-10-25 17:34:31 +0800

    feat: deploy

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ git commit -m 'deploy'
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
-
+# git remote add origin https://github.com/pinky-pig/what-is-my-fabric.git
 # git push -f https://github.com/pinky-pig/what-is-my-blog.git master:gh-pages
 
 # 返回目录，将刚才打包生成的静态文件在目录中删除
