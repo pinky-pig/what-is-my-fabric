f83573506befc348efce3e1322226ec3583fefeb
pinky-pig <2545589993@qq.com>
2022-11-05 16:47:12 +0800

    fix: 删除无用代码解决build问题

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,15 +7,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    CardGroup: typeof import('./components/CardGroup.vue')['default']
-    ConfettiCanvas: typeof import('./components/ConfettiCanvas.vue')['default']
-    CustomCard: typeof import('./components/CustomCard.vue')['default']
-    Footer: typeof import('./components/Footer.vue')['default']
-    Header: typeof import('./components/Header.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
-    NaiveProvider: typeof import('./components/NaiveProvider.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    Side: typeof import('./components/Side.vue')['default']
   }
 }
--- src/components/ConfettiCanvas.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <canvas
-    id="confetti"
-    class=" absolute top-0 left-0 w-full h-full z-20"
-  />
-</template>
