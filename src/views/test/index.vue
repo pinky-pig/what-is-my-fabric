e7b7cc6901899024902ec652fe0efa57c0d2c998
pinky-pig <2545589993@qq.com>
2022-11-12 16:41:06 +0800

    feat: 添加控制点在曲线上的贝塞尔曲线

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,28 +1,37 @@
 <script setup lang="ts">
-
+const middle = ref(null)
+const end = ref(null)
+const pointStart = ref([0, 100])
+const pointEnd = ref([800, 100])
+const pointMiddle = ref([600, 0])
+const p = computed(() => {
+  return `
+    M ${pointStart.value[0]} ${pointStart.value[1]}
+    C ${pointStart.value[0]} ${pointStart.value[1]} ${pointMiddle.value[0] - 100} ${pointMiddle.value[1]} ${pointMiddle.value[0]} ${pointMiddle.value[1]}
+    S ${pointEnd.value[0]} ${pointEnd.value[1]} ${pointEnd.value[0]} ${pointEnd.value[1]}
+  `
+})
+let isDragging = false
+const handleMove = (e) => {
+  if (isDragging)
+    pointMiddle.value = [e.pageX, e.pageY]
+}
+const handleDown = () => {
+  isDragging = true
+}
+const handleUp = () => {
+  isDragging = false
+}
 </script>
 
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-    <g>
-      <path
-        id="path" d="M10 10 S 10 75 50 50" fill="transparent" stroke-width="0.5" stroke="#000"
-        stroke-miterlimit="10"
-      />
-    </g>
-
-    <circle cx="10" cy="10" r="1" stroke-width="0.2" fill="red" />
-    <circle cx="50" cy="50" r="1" stroke-width="0.2" fill="red" />
-    <g class="pen">
-      <circle id="curve" cx="10" cy="75" r="1" stroke-width="0.2" fill="green" />
-
-      <path id="path-curve" d="M10 75 L 50 50" stroke="green" stroke-miterlimit="1" stroke-width="0.2" />
-
-      <circle id="cursor" cx="90" cy="25" r="1" stroke-width="0.2" fill="rebeccapurple" />
-
-      <path id="path-cursor" d="M50 50 L 90 25" stroke="blue" stroke-miterlimit="1" stroke-width="0.2" />
-    </g>
-
+  <svg @mousemove="handleMove" @mouseup="handleUp">
+    <path :d="p" fill="transparent" stroke="black" />
+    <circle ref="start" :cx="pointStart[0]" :cy="pointStart[1]" r="10" stroke-width="0.2" fill="red" />
+    <circle ref="end" :cx="pointEnd[0]" :cy="pointEnd[1]" r="10" stroke-width="0.2" fill="red" />
+    <circle ref="control1" :cx="pointMiddle[0] - 100" :cy="pointMiddle[1] " r="10" stroke-width="0.2" fill="green" />
+    <circle ref="control1" :cx="pointMiddle[0] + 100" :cy="pointMiddle[1] " r="10" stroke-width="0.2" fill="blue" />
+    <circle ref="middle" :cx="pointMiddle[0]" :cy="pointMiddle[1]" r="20" stroke-width="0.2" fill="red" @mousedown="handleDown" />
   </svg>
 </template>
 
@@ -32,11 +41,13 @@ body {
   display: flex;
   justify-content: center;
 }
-
 svg {
-  margin: 0 auto;
-  width: 400px;
-  background: 400px;
-  border: 1px solid white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: #ffffff;
+  touch-action: none;
 }
 </style>
