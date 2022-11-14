d8f01493f356db9ceeadc451b6a2100fdc4cd484
pinky-pig <2545589993@qq.com>
2022-11-14 17:51:26 +0800

    feat: 添加箭头方向

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 const middle = ref(null)
 const end = ref(null)
-const pointStart = ref([0, 100])
-const pointEnd = ref([800, 100])
-const pointMiddle = ref([600, 0])
+const pointStart = ref([50, 200])
+const pointEnd = ref([800, 200])
+const pointMiddle = ref([600, 100])
 const p = computed(() => {
   return `
     M ${pointStart.value[0]} ${pointStart.value[1]}
@@ -12,16 +12,56 @@ const p = computed(() => {
   `
 })
 
-const calArrowHeadCoords = (point: number[], h = 10) => {
+// 计算箭头两翼的
+const calArrowHeadCoords = (point: number[], h = 50) => {
   // 设置角度为30度
   const x = h * Math.sqrt(3)
   return [
-    [x, point[1] + h],
-    [x, point[1] - h],
+    [point[0] + x, point[1] + h],
+    [point[0] + x, point[1] - h],
   ]
 }
 
-const arrowHeadPath = computed(() => {
+// 计算S控制点与终点的角度
+const calAngleControlAndEnd = (start: any, end: any) => {
+  const distanceX = end[0] - start[0]
+  const distanceY = end[1] - start[1]
+  const baseAngle = Math.atan2(distanceY, distanceX)
+  return baseAngle
+}
+
+const arrowHeadPath = ref()
+watch(() => pointMiddle.value, () => {
+  const angle = calAngleControlAndEnd([pointEnd.value[0], pointEnd.value[1]], [pointMiddle.value[0] + 100, pointMiddle.value[1]])
+  const coords = calArrowHeadCoords(pointEnd.value)
+  const c = coords.map((item) => {
+    return calculateCoords(pointEnd.value, item, angle)
+  })
+  arrowHeadPath.value = `
+    M ${pointEnd.value[0]} ${pointEnd.value[1]}
+    L ${c[0][0]} ${c[0][1]}
+    M ${pointEnd.value[0]} ${pointEnd.value[1]}
+    L ${c[1][0]} ${c[1][1]}
+  `
+}, {
+  immediate: true,
+})
+
+function calculateCoords(start: number[], end: number[], angle) {
+  const x = start[0]
+  const y = start[1]
+  const x1 = end[0]
+  const y1 = end[1]
+
+  const sin = Math.sin(angle)
+  const cos = Math.cos(angle)
+
+  const x2 = x + (x1 - x) * cos - (y1 - y) * sin
+  const y2 = y + (y1 - y) * cos + (x1 - x) * sin
+  return [x2, y2]
+}
+
+const defaultArrowHeadPath = computed(() => {
   const coords = calArrowHeadCoords(pointEnd.value)
 
   return `
@@ -31,22 +71,6 @@ const arrowHeadPath = computed(() => {
   L ${coords[1][0]} ${coords[1][1]}
   `
 })
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-// const calArrowHeadOrientation = (pointControl: [number, number], point: [number, number]) => {
-//   const angle = calculateAngle(pointControl, point)
-//   const coords = calArrowHeadCoords(pointEnd.value)
-//   coords = coords.map((item: number[]) => {
-//     return calculateCoords(item, angle)
-//   })
-// }
-// function calculateAngle(start: [number, number], end: [number, number]) {
-//   const distanceX = end[0] - start[0]
-//   const distanceY = end[1] - start[1]
-//   const baseAngle = Math.atan2(distanceY, distanceX)
-//   return baseAngle
-// }
-////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let isDragging = false
 const handleMove = (e) => {
@@ -63,8 +87,10 @@ const handleUp = () => {
 
 <template>
   <svg @mousemove="handleMove" @mouseup="handleUp">
-    <path :d="p" fill="transparent" stroke="black" />
-    <path :d="arrowHeadPath" fill="transparent" stroke="black" />
+    <path :d="p" fill="transparent" stroke="purple" />
+    <path :d="arrowHeadPath" fill="transparent" stroke="purple" />
+    <path :d="defaultArrowHeadPath" fill="transparent" stroke="black" />
+
     <circle ref="start" :cx="pointStart[0]" :cy="pointStart[1]" r="10" stroke-width="0.2" fill="red" />
     <circle ref="end" :cx="pointEnd[0]" :cy="pointEnd[1]" r="10" stroke-width="0.2" fill="red" />
     <circle ref="control1" :cx="pointMiddle[0] - 100" :cy="pointMiddle[1] " r="10" stroke-width="0.2" fill="green" />
