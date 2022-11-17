0a1d22f87836b7cd15222323006364b540f0242b
pinky-pig <2545589993@qq.com>
2022-11-17 18:58:26 +0800

    refactor: 监听拖拽画布

--- src/views/whiteboard/components/canvas/index.vue
+++ src/views/whiteboard/components/canvas/index.vue
@@ -6,6 +6,7 @@ import { getSvgPathFromStroke } from '../../../utils/index'
 import { isPressedCtrl, useKeyEvents } from '../../hooks/useKeyEvents'
 import { useZoomEvents } from '../../hooks/useZoomEvents'
 import { useResizeObserver } from '../../hooks/useResizeObserver'
+import { useCanvasEvents } from '../../hooks/useCanvasEvents'
 import { useSvgStore } from '~/store/modules/svg'
 
 // free-hand的配置
@@ -41,76 +42,12 @@ onMounted(() => {
   setViewPort()
 })
 
-let draggedEvt: any = null // 每次的拖拽事件
-const drag = (event: MouseEvent | TouchEvent) => {
-  const pt = eventToLocation(event)
-  if (draggedEvt) {
-    const oriPt = eventToLocation(draggedEvt)
-    const x = cfg.value.viewPortX + (oriPt.x - pt.x)
-    const y = cfg.value.viewPortY + (oriPt.y - pt.y)
-    const w = cfg.value.viewPortWidth
-    const h = cfg.value.viewPortHeight
-
-    updateViewPort(x, y, w, h)
-  }
-  draggedEvt = event
-}
-
-function eventToLocation(event: MouseEvent | TouchEvent, idx = 0): { x: number; y: number } {
-  const { top, left } = useElementBounding(svgWrapperRef)
-  const touch = event instanceof MouseEvent ? event : event.touches[idx]
-  const x = cfg.value.viewPortX + (touch.clientX - left.value) * viewPortZoom.value
-  const y = cfg.value.viewPortY + (touch.clientY - top.value) * viewPortZoom.value
-  return { x, y }
-}
-
-function updateViewPort(x: number, y: number, w: number | null, h: number | null, force = false) {
-  if (!svgWrapperRef.value)
-    return
-
-  // 这里的w,h比例应该跟画布的比例相同。虽然在调用这个方法之前计算的w、h也是根据比例计算的。
-  // 这里经过下面的计算不出意外得出的值应该跟其之前相同
-  if (w === null && h !== null)
-    w = svgWrapperRef.value.offsetWidth * h / svgWrapperRef.value.offsetHeight
-
-  if (h === null && w !== null)
-    h = svgWrapperRef.value.offsetHeight * w / svgWrapperRef.value.offsetWidth
-
-  if (!w || !h)
-    return
-  cfg.value.viewPortX = parseFloat((1 * x).toPrecision(6))
-  cfg.value.viewPortY = parseFloat((1 * y).toPrecision(6))
-  cfg.value.viewPortWidth = parseFloat((1 * w).toPrecision(4))
-  cfg.value.viewPortHeight = parseFloat((1 * h).toPrecision(4))
-  viewPortZoom.value = cfg.value.viewPortWidth / svgWrapperRef.value.offsetWidth
-}
-
-function handlePointerDown(e) {
-  e.target.setPointerCapture(e.pointerId)
-  const pt = eventToLocation(e)
-  freeDrawPoints.value = [[pt.x, pt.y, e.pressure]]
-}
-function handlePointerMove(e) {
-  if (isPressedCtrl.value) {
-    // 移动画布
-    drag(e)
-  }
-  else {
-    if (e.buttons !== 1)
-      return
-
-    const pt = eventToLocation(e)
-    freeDrawPoints.value = [...freeDrawPoints.value, [pt.x, pt.y, e.pressure]]
-  }
-}
-function handlePointerUp(e) {
-  draggedEvt = null
-}
-
 // 监听画布缩放
 useZoomEvents(cfg, svgWrapperRef, viewPortZoom)
 // 监听窗口尺寸改变
 useResizeObserver(cfg, svgWrapperRef, viewPortZoom)
+// 监听画布触发事件（包括拖拽画布）
+useCanvasEvents(cfg, svgWrapperRef, viewPortZoom, freeDrawPoints)
 </script>
 
 <template>
@@ -120,9 +57,6 @@ useResizeObserver(cfg, svgWrapperRef, viewPortZoom)
       ref="svgCanvasRef"
       :viewBox="`${cfg.viewPortX} ${cfg.viewPortY} ${cfg.viewPortWidth} ${cfg.viewPortHeight}`"
       class="w-full h-full"
-      @pointerdown="handlePointerDown"
-      @pointermove="handlePointerMove"
-      @pointerup="handlePointerUp"
     >
       <path :d="pathData" />
     </svg>
--- src/views/whiteboard/hooks/useCanvasEvents.ts
+++ src/views/whiteboard/hooks/useCanvasEvents.ts
@@ -1,29 +1,94 @@
-import { useSvgStore } from '~/store/modules/svg'
+import type { Ref } from 'vue'
+import { isPressedCtrl } from './useKeyEvents'
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// export function useCanvasEvents() {
-//   let draggedEvt: any = null // 每次的拖拽事件
-
-//   function handlePointerDown(e: PointerEvent) {
-//     if (!e.target)
-//       return
-//     const pt = eventToLocation(e)
-//     points.value = [[pt.x, pt.y, e.pressure]]
-//   }
-//   function handlePointerMove(e: PointerEvent) {
-
-//   }
-//   function handlePointerUp(e: PointerEvent) {
-//     draggedEvt = null
-//   }
-
-//   return { handlePointerDown, handlePointerMove, handlePointerUp }
-// }
-export function eventToLocation(event: MouseEvent | TouchEvent, idx = 0): { x: number; y: number } {
-  const store = useSvgStore()
-  const { top, left } = useElementBounding(store.svgWrapperRef)
-  const touch = event instanceof MouseEvent ? event : event.touches[idx]
-  const x = store.cfg.viewPortX + (touch.clientX - left.value) * store.viewPortZoom
-  const y = store.cfg.viewPortY + (touch.clientY - top.value) * store.viewPortZoom
-  return { x, y }
+interface cfgType {
+  viewPortX: number
+  viewPortY: number
+  viewPortWidth: number
+  viewPortHeight: number
 }
+
+let draggedEvt: MouseEvent | TouchEvent | null = null
+/**
+   * 监听画布上的事件 （ onPointerDown|onPointerMove|onPointerUp ）
+   * @param cfgRef 需要改变的缩放值
+   * @param canvasRef 缩放画板DOM
+   * @param viewPortZoomRef 缩放层级
+   */
+export function useCanvasEvents(
+  cfgRef: Ref<cfgType>,
+  canvasRef: Ref<HTMLDivElement | null>,
+  viewPortZoomRef: Ref<number>,
+  freeDrawPointsRef: Ref<(number[] | {
+    x: number
+    y: number
+    pressure?: number
+  })[]>,
+) {
+  // 更新设置viewBox的值（响应式，由参数传进来）
+  function updateViewPort(x: number, y: number, w: number | null, h: number | null) {
+    if (!canvasRef.value)
+      return
+    if (w === null && h !== null)
+      w = canvasRef.value.offsetWidth * h / canvasRef.value.offsetHeight
+
+    if (h === null && w !== null)
+      h = canvasRef.value.offsetHeight * w / canvasRef.value.offsetWidth
+
+    if (!w || !h)
+      return
+    cfgRef.value.viewPortX = parseFloat((1 * x).toPrecision(6))
+    cfgRef.value.viewPortY = parseFloat((1 * y).toPrecision(6))
+    cfgRef.value.viewPortWidth = parseFloat((1 * w).toPrecision(4))
+    cfgRef.value.viewPortHeight = parseFloat((1 * h).toPrecision(4))
+    viewPortZoomRef.value = cfgRef.value.viewPortWidth / canvasRef.value.offsetWidth
+  }
+  // 将鼠标的绝对位置转换成在画布中的位置
+  function eventToLocation(event: MouseEvent | TouchEvent, idx = 0): { x: number; y: number } {
+    const { top, left } = useElementBounding(canvasRef)
+    const touch = event instanceof MouseEvent ? event : event.touches[idx]
+    const x = cfgRef.value.viewPortX + (touch.clientX - left.value) * viewPortZoomRef.value
+    const y = cfgRef.value.viewPortY + (touch.clientY - top.value) * viewPortZoomRef.value
+    return { x, y }
+  }
+
+  function dragCanvas(event: MouseEvent | TouchEvent) {
+    const pt = eventToLocation(event)
+    if (draggedEvt) {
+      const oriPt = eventToLocation(draggedEvt)
+      const x = cfgRef.value.viewPortX + (oriPt.x - pt.x)
+      const y = cfgRef.value.viewPortY + (oriPt.y - pt.y)
+      const w = cfgRef.value.viewPortWidth
+      const h = cfgRef.value.viewPortHeight
+
+      updateViewPort(x, y, w, h)
+    }
+    draggedEvt = event
+  }
+
+  function handlePointerDown(e: PointerEvent) {
+    const pt = eventToLocation(e)
+    freeDrawPointsRef.value = [[pt.x, pt.y, e.pressure]]
+  }
+  function handlePointerMove(e: PointerEvent) {
+    if (isPressedCtrl.value) {
+      // 移动画布
+      dragCanvas(e)
+    }
+    else {
+      if (e.buttons !== 1)
+        return
+      const pt = eventToLocation(e)
+      freeDrawPointsRef.value = [...freeDrawPointsRef.value, [pt.x, pt.y, e.pressure]]
+    }
+  }
+  function handlePointerUp() {
+    draggedEvt = null
+  }
+
+  // 监听鼠标滚轮事件
+  useEventListener(canvasRef, 'pointerdown', handlePointerDown)
+  useEventListener(canvasRef, 'pointermove', handlePointerMove)
+  useEventListener(canvasRef, 'pointerup', handlePointerUp)
+}
+
