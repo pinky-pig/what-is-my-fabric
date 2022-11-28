b8d0560142f568e4eadc8e060f12e908219eb154
pinky-pig <2545589993@qq.com>
2022-11-28 19:48:27 +0800

    refactor: 重写useBounds.ts

--- src/views/whiteboard/hooks/useBounds/useBounds.ts
+++ src/views/whiteboard/hooks/useBounds/useBounds.ts
@@ -2,8 +2,9 @@ import { storeToRefs } from 'pinia'
 // import { Rectangle } from '../components/element'
 // import { generateUuid } from '../utils'
 import type { Ref } from 'vue'
-import { generateUuid } from '../utils'
-import { distanceBetweenPointAndSegment } from '../utils/math'
+
+import { generateUuid } from '../../utils'
+import { hitTest } from './hitTest'
 import type { CurrentElementType } from '~/store/modules/svg'
 import { useSvgStore } from '~/store/modules/svg'
 
@@ -103,79 +104,3 @@ export function useBoundsBox(selectedBounds: Ref<ElementBound[]>) {
     return { x, y }
   }
 }
-
-function hitTest(
-  element: any,
-  x: number,
-  y: number,
-) {
-  // For shapes that are composed of lines, we only enable point-selection when the distance
-  // of the click is less than x pixels of any of the lines that the shape is composed of
-  const lineThreshold = 10
-
-  if (element.type === 'Ellipse') {
-    // https://stackoverflow.com/a/46007540/232122
-    const px = Math.abs(x - element.bound.x - element.bound.width / 2)
-    const py = Math.abs(y - element.bound.y - element.bound.height / 2)
-
-    let tx = 0.707
-    let ty = 0.707
-
-    const a = element.bound.width / 2
-    const b = element.bound.height / 2;
-
-    [0, 1, 2, 3].forEach(() => {
-      const xx = a * tx
-      const yy = b * ty
-
-      const ex = ((a * a - b * b) * tx ** 3) / a
-      const ey = ((b * b - a * a) * ty ** 3) / b
-
-      const rx = xx - ex
-      const ry = yy - ey
-
-      const qx = px - ex
-      const qy = py - ey
-
-      const r = Math.hypot(ry, rx)
-      const q = Math.hypot(qy, qx)
-
-      tx = Math.min(1, Math.max(0, ((qx * r) / q + ex) / a))
-      ty = Math.min(1, Math.max(0, ((qy * r) / q + ey) / b))
-      const t = Math.hypot(ty, tx)
-      tx /= t
-      ty /= t
-    })
-
-    return Math.hypot(a * tx - px, b * ty - py) < lineThreshold
-  }
-  else if (element.type === 'Rectangle') {
-    const x1 = getElementAbsoluteX1(element.bound)
-    const x2 = getElementAbsoluteX2(element.bound)
-    const y1 = getElementAbsoluteY1(element.bound)
-    const y2 = getElementAbsoluteY2(element.bound)
-    // (x1, y1) --A-- (x2, y1)
-    //    |D             |B
-    // (x1, y2) --C-- (x2, y2)
-    return (
-      distanceBetweenPointAndSegment(x, y, x1, y1, x2, y1) < lineThreshold // A
-      || distanceBetweenPointAndSegment(x, y, x2, y1, x2, y2) < lineThreshold // B
-      || distanceBetweenPointAndSegment(x, y, x2, y2, x1, y2) < lineThreshold // C
-      || distanceBetweenPointAndSegment(x, y, x1, y2, x1, y1) < lineThreshold // D
-    )
-  }
-}
-
-export function getElementAbsoluteX1(element: any) {
-  return element.width >= 0 ? element.x : element.x + element.width
-}
-export function getElementAbsoluteX2(element: any) {
-  return element.width >= 0 ? element.x + element.width : element.x
-}
-export function getElementAbsoluteY1(element: any) {
-  return element.height >= 0 ? element.y : element.y + element.height
-}
-export function getElementAbsoluteY2(element: any) {
-  return element.height >= 0 ? element.y + element.height : element.y
-}
-
