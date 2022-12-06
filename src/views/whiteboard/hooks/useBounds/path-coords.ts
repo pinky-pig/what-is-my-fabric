95b5ecb26c86c909149ddabc9433e715aa262ffe
pinky-pig <2545589993@qq.com>
2022-12-06 18:29:50 +0800

    refactor: 重构 transform-parser

---
+++ src/views/whiteboard/hooks/useBounds/path-coords.ts
@@ -0,0 +1,115 @@
+/* eslint-disable eqeqeq */
+export function transformPoint(x, y, m) {
+  return { x: m.a * x + m.c * y + m.e, y: m.b * x + m.d * y + m.f }
+}
+const pathMap = [0, 'z', 'M', 'm', 'L', 'l', 'C', 'c', 'Q', 'q', 'A', 'a',
+  'H', 'h', 'V', 'v', 'S', 's', 'T', 't']
+export function remapPath(selected: SVGPathElement & { pathSegList: any }, m: any) {
+  const remap = (x, y) => transformPoint(x, y, m)
+  const scaleW = w => m.a * w
+  const scaleH = h => m.d * h
+  // 1.获取path的点位。e.g. 有7个点，获取到的numberOfItems也是7
+  const segList = selected.pathSegList
+  const len = segList.numberOfItems
+  // 2.初始化长度的数组
+  const changes = {
+    d: new Array(len),
+  }
+  // changes.d = new Array(len)
+  for (let i = 0; i < len; ++i) {
+    const seg = segList.getItem(i)
+    changes.d[i] = {
+      type: seg.pathSegType,
+      x: seg.x,
+      y: seg.y,
+      x1: seg.x1,
+      y1: seg.y1,
+      x2: seg.x2,
+      y2: seg.y2,
+      r1: seg.r1,
+      r2: seg.r2,
+      angle: seg.angle,
+      largeArcFlag: seg.largeArcFlag,
+      sweepFlag: seg.sweepFlag,
+    }
+  }
+  const firstSeg = changes.d[0]
+  const currentPt = remap(firstSeg.x, firstSeg.y)
+  changes.d[0].x = currentPt.x
+  changes.d[0].y = currentPt.y
+  for (let i = 1; i < len; ++i) {
+    const seg = changes.d[i]
+    const type = seg.type
+    // if absolute or first segment, we want to remap x, y, x1, y1, x2, y2
+    // if relative, we want to scaleW, scaleH
+    if (type % 2 == 0) { // absolute
+      const thisx = (seg.x != undefined) ? seg.x : currentPt.x // for V commands
+      const thisy = (seg.y != undefined) ? seg.y : currentPt.y // for H commands
+      const pt = remap(thisx, thisy)
+      const pt1 = remap(seg.x1, seg.y1)
+      const pt2 = remap(seg.x2, seg.y2)
+      seg.x = pt.x
+      seg.y = pt.y
+      seg.x1 = pt1.x
+      seg.y1 = pt1.y
+      seg.x2 = pt2.x
+      seg.y2 = pt2.y
+      seg.r1 = scaleW(seg.r1)
+      seg.r2 = scaleH(seg.r2)
+    }
+    else { // relative
+      seg.x = scaleW(seg.x)
+      seg.y = scaleH(seg.y)
+      seg.x1 = scaleW(seg.x1)
+      seg.y1 = scaleH(seg.y1)
+      seg.x2 = scaleW(seg.x2)
+      seg.y2 = scaleH(seg.y2)
+      seg.r1 = scaleW(seg.r1)
+      seg.r2 = scaleH(seg.r2)
+    }
+  } // for each segment
+
+  let dStr = ''
+  for (let i = 0; i < len; ++i) {
+    const seg = changes.d[i]
+    const type = seg.type
+    dStr += pathMap[type]
+    switch (type) {
+      case 13: // relative horizontal line (h)
+      case 12: // absolute horizontal line (H)
+        dStr += `${seg.x} `
+        break
+      case 15: // relative vertical line (v)
+      case 14: // absolute vertical line (V)
+        dStr += `${seg.y} `
+        break
+      case 3: // relative move (m)
+      case 5: // relative line (l)
+      case 19: // relative smooth quad (t)
+      case 2: // absolute move (M)
+      case 4: // absolute line (L)
+      case 18: // absolute smooth quad (T)
+        dStr += `${seg.x},${seg.y} `
+        break
+      case 7: // relative cubic (c)
+      case 6: // absolute cubic (C)
+        dStr += `${seg.x1},${seg.y1} ${seg.x2},${seg.y2} ${
+               seg.x},${seg.y} `
+        break
+      case 9: // relative quad (q)
+      case 8: // absolute quad (Q)
+        dStr += `${seg.x1},${seg.y1} ${seg.x},${seg.y} `
+        break
+      case 11: // relative elliptical arc (a)
+      case 10: // absolute elliptical arc (A)
+        dStr += `${seg.r1},${seg.r2} ${seg.angle} ${+seg.largeArcFlag
+              } ${+seg.sweepFlag} ${seg.x},${seg.y} `
+        break
+      case 17: // relative smooth cubic (s)
+      case 16: // absolute smooth cubic (S)
+        dStr += `${seg.x2},${seg.y2} ${seg.x},${seg.y} `
+    }
+  }
+
+  return dStr
+}
--- src/views/whiteboard/hooks/useBounds/transform-parser.ts
+++ src/views/whiteboard/hooks/useBounds/transform-parser.ts
@@ -1,15 +1,5 @@
-/* eslint-disable eqeqeq */
 import 'pathseg'
-export const translateRegExp = /translate\((.*)[\s\,\s](.*)\)\s*/
-
-export function getTranslateFromString(str: string) {
-  const translate = str.replace(/\s+/g, '')
-    .match(translateRegExp)
-  if (translate)
-    return [translate[1], translate[2]]
-  else
-    return [0, 0]
-}
+import { remapPath } from './path-coords'
 
 export function getTransformList(elem: any) {
   if (elem.transform)
@@ -24,9 +14,6 @@ export function getTransformList(elem: any) {
   return null
 }
 
-export function transformPoint(x, y, m) {
-  return { x: m.a * x + m.c * y + m.e, y: m.b * x + m.d * y + m.f }
-}
 export function matrixMultiply(...arg: any[]) {
   const args = arg
   const NEAR_ZERO = 1e-14
@@ -50,8 +37,7 @@ export function matrixMultiply(...arg: any[]) {
 
   return m
 }
-const pathMap = [0, 'z', 'M', 'm', 'L', 'l', 'C', 'c', 'Q', 'q', 'A', 'a',
-  'H', 'h', 'V', 'v', 'S', 's', 'T', 't']
+
 export function transformListToTransform(tlist, mi?: any, ma?: any) {
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
   if (tlist === null) {
@@ -73,13 +59,15 @@ export function transformListToTransform(tlist, mi?: any, ma?: any) {
   }
   return svg.createSVGTransformFromMatrix(m)
 }
+// 画布的变形。本项目没有transform，所以一直都是 1 0 0 1 0 0
 const svgRoot = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-
 export function recalculateDimensions(selected: SVGPathElement) {
-  // 主要是为了获取transform的矩阵
+  // 1.获取默认矩阵
   let m = svgRoot.createSVGMatrix()
+  // 2.获取变形的数组
   const tlist = getTransformList(selected)
   const N = tlist.numberOfItems
+  // 3.将transform转成矩阵
   if ((N === 1 || (N > 1 && tlist.getItem(1).type !== 3)) && tlist.getItem(0).type === 2) {
     const oldXLate = tlist.getItem(0).matrix
     const meq = transformListToTransform(tlist, 1).matrix
@@ -87,111 +75,6 @@ export function recalculateDimensions(selected: SVGPathElement) {
     m = matrixMultiply(meq_inv, oldXLate, meq)
     tlist.removeItem(0)
   }
-  // 如果是平移或者缩放，需要重新计算
-  return remapPath(selected, { d: selected.getAttribute('d') }, m)
-}
-
-export function remapPath(selected: any, changes: any, m: any) {
-  const remap = (x, y) => transformPoint(x, y, m)
-  const scaleW = w => m.a * w
-  const scaleH = h => m.d * h
-  const segList = selected.pathSegList
-  const len = segList.numberOfItems
-  changes.d = new Array(len)
-  for (let i = 0; i < len; ++i) {
-    const seg = segList.getItem(i)
-    changes.d[i] = {
-      type: seg.pathSegType,
-      x: seg.x,
-      y: seg.y,
-      x1: seg.x1,
-      y1: seg.y1,
-      x2: seg.x2,
-      y2: seg.y2,
-      r1: seg.r1,
-      r2: seg.r2,
-      angle: seg.angle,
-      largeArcFlag: seg.largeArcFlag,
-      sweepFlag: seg.sweepFlag,
-    }
-  }
-  const firstSeg = changes.d[0]
-  const currentPt = remap(firstSeg.x, firstSeg.y)
-  changes.d[0].x = currentPt.x
-  changes.d[0].y = currentPt.y
-  for (let i = 1; i < len; ++i) {
-    const seg = changes.d[i]
-    const type = seg.type
-    // if absolute or first segment, we want to remap x, y, x1, y1, x2, y2
-    // if relative, we want to scaleW, scaleH
-    if (type % 2 == 0) { // absolute
-      const thisx = (seg.x != undefined) ? seg.x : currentPt.x // for V commands
-      const thisy = (seg.y != undefined) ? seg.y : currentPt.y // for H commands
-      const pt = remap(thisx, thisy)
-      const pt1 = remap(seg.x1, seg.y1)
-      const pt2 = remap(seg.x2, seg.y2)
-      seg.x = pt.x
-      seg.y = pt.y
-      seg.x1 = pt1.x
-      seg.y1 = pt1.y
-      seg.x2 = pt2.x
-      seg.y2 = pt2.y
-      seg.r1 = scaleW(seg.r1)
-      seg.r2 = scaleH(seg.r2)
-    }
-    else { // relative
-      seg.x = scaleW(seg.x)
-      seg.y = scaleH(seg.y)
-      seg.x1 = scaleW(seg.x1)
-      seg.y1 = scaleH(seg.y1)
-      seg.x2 = scaleW(seg.x2)
-      seg.y2 = scaleH(seg.y2)
-      seg.r1 = scaleW(seg.r1)
-      seg.r2 = scaleH(seg.r2)
-    }
-  } // for each segment
-
-  let dStr = ''
-  for (let i = 0; i < len; ++i) {
-    const seg = changes.d[i]
-    const type = seg.type
-    dStr += pathMap[type]
-    switch (type) {
-      case 13: // relative horizontal line (h)
-      case 12: // absolute horizontal line (H)
-        dStr += `${seg.x} `
-        break
-      case 15: // relative vertical line (v)
-      case 14: // absolute vertical line (V)
-        dStr += `${seg.y} `
-        break
-      case 3: // relative move (m)
-      case 5: // relative line (l)
-      case 19: // relative smooth quad (t)
-      case 2: // absolute move (M)
-      case 4: // absolute line (L)
-      case 18: // absolute smooth quad (T)
-        dStr += `${seg.x},${seg.y} `
-        break
-      case 7: // relative cubic (c)
-      case 6: // absolute cubic (C)
-        dStr += `${seg.x1},${seg.y1} ${seg.x2},${seg.y2} ${
-               seg.x},${seg.y} `
-        break
-      case 9: // relative quad (q)
-      case 8: // absolute quad (Q)
-        dStr += `${seg.x1},${seg.y1} ${seg.x},${seg.y} `
-        break
-      case 11: // relative elliptical arc (a)
-      case 10: // absolute elliptical arc (A)
-        dStr += `${seg.r1},${seg.r2} ${seg.angle} ${+seg.largeArcFlag
-              } ${+seg.sweepFlag} ${seg.x},${seg.y} `
-        break
-      case 17: // relative smooth cubic (s)
-      case 16: // absolute smooth cubic (S)
-        dStr += `${seg.x2},${seg.y2} ${seg.x},${seg.y} `
-    }
-  }
-
-  return dStr
+  // 4.将要变换的要素及其变化后的要素的矩阵传入生成新的路径
+  return remapPath(selected as SVGPathElement & { pathSegList: any }, m)
 }
